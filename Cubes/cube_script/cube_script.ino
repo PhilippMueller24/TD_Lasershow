49306f3c020946d427f4291d06467a731b59bcbc
PhilippMueller24 <54212910+PhilippMueller24@users.noreply.github.com>
2025-05-01 21:46:24 +0200

    aftermath mühle24

--- Cubes/cube_script/cube_script.ino
+++ Cubes/cube_script/cube_script.ino
@@ -1,52 +1,41 @@
-#include <PololuLedStrip.h>
+//einbinden der Bibliotheken zum
+//ansteuern der LED Matrix auf dem
+//Arduino UNO R4 WiFi
+#include "WIFI.h"
+#include "Arduino_LED_Matrix.h"
 
-// Create an ledStrip object and specify the pin it will use.
-PololuLedStrip<12> ledStrip;
+//erzeugen eines ArduinoLEDMatrix Objektes
+ArduinoLEDMatrix matrix;
 
-// Create a buffer for holding the colors (3 bytes per color).
-#define LED_COUNT 255
-rgb_color colors[LED_COUNT];
-
-void setup()
-{
-  // Start up the serial port, for communication with the PC.
-  Serial.begin(115200);
-  Serial.println("Ready to receive colors.");
+void setup() {
+  //beginn der kommunikation mit der LED Matrix
+  matrix.begin();
 }
 
-void loop()
-{
-  // If any digit is received, we will go into integer parsing mode
-  // until all three calls to parseInt return an interger or time out.
-  if (Serial.available())
-  {
-    char c = Serial.peek();
-    if (!(c >= '0' && c <= '9'))
-    {
-      Serial.read(); // Discard non-digit character
-    }
-    else
-    {
-      rgb_color color;
-      for (uint16_t i = 0; i < LED_COUNT; i++)
-      {
+void loop() {
+  //beginn des zeichnens
+  matrix.beginDraw();
+  //alle LEDs deaktivieren
+  matrix.stroke(0xFFFFFFFF);
+  //Geschwindigkeit des Scrolltextes setzen
+  //je niedriger der Wert desto schneller läuft der Text
+  matrix.textScrollSpeed(50);
 
-        color.red = Serial.parseInt();
-        color.green = Serial.parseInt();
-        color.blue = Serial.parseInt();
-
-        colors[i] = color;
-      }
-
-      // Write to the LED strip.
-      ledStrip.write(colors, LED_COUNT);
-
-      Serial.print("Showing color: ");
-      Serial.print(color.red);
-      Serial.print(",");
-      Serial.print(color.green);
-      Serial.print(",");
-      Serial.println(color.blue);
-    }
-  }
+  //der Text welcher ausgegeben werden soll
+  const char text[] = "    Hallo Welt!    ";
+  //Schriftgröße setzen, 
+  //verfügbare Werte sind:
+  // - Font_4x6
+  // - Font_5x7
+  matrix.textFont(Font_4x6);
+  //koordinate für den Text
+  //je nach Schriftgröße kann man damit den Text
+  //in der Matrix zentrieren
+  matrix.beginText(0, 1, 0xFFFFFF);
+  //Ausgeben des Textes
+  matrix.println(text);
+  //definieren von wo gescrollt werden soll
+  matrix.endText(SCROLL_LEFT);
+  //beenden des zeichnens
+  matrix.endDraw();
 }
